ad1e228acdfcb0fb31a890f70262fca3c26c9215
ji-hoooon <ljh6384@naver.com>
2023-04-19 20:54:39 +0900

    ADD init commit

---
+++ src/test/java/com/mino/blogproj/BlogprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mino.blogproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
